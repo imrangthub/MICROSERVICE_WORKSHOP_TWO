f04c06935074977344242159be2f71808b0b6b63
MD IMRAN HOSSAIN <imranmadbar@gmail.com>
2021-09-11 09:40:35 +0600

    Upadte depandency

--- movie-catalog-service/src/main/java/com/imranmadbar/MovieCatalogApp.java
+++ movie-catalog-service/src/main/java/com/imranmadbar/MovieCatalogApp.java
@@ -6,9 +6,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class MovieCatalogApp {
 
 
