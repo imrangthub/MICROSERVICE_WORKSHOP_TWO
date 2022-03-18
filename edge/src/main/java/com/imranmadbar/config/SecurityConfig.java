bcd7b693ce3572cc8660e4de9247b9792e02623d
MD_IMRAN_HOSSAIN <imranmadbar@gmail.com>
2022-03-18 15:27:04 +0600

    Update spring sec

---
+++ edge/src/main/java/com/imranmadbar/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+//package com.imranmadbar.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//
+//@Configuration
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//
+//
+//    @Override
+//    public void configure(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity
+//                .authorizeRequests()
+//                .antMatchers("/ping**").permitAll()
+//                .antMatchers("/home**").permitAll()
+//                .antMatchers(
+//                        "/v2/api-docs",
+//                        "/swagger-ui/**",
+//                        "/swagger-ui.html",
+//                        "/swagger-resources/**",
+//                        "/*/swagger-resources/**",
+//                        "/*/v2/api-docs")
+//                .permitAll()
+//                .and()
+//                .authorizeRequests()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .cors()
+//                .and()
+//                .csrf()
+//                .disable()
+//                .oauth2ResourceServer()
+//                .jwt();
+//    }
+//
+//}
+//
