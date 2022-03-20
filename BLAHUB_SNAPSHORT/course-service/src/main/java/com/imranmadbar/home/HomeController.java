d0098566a997a5c3ace0eb7dd0ee301fbe414585
MD_IMRAN_HOSSAIN <imranmadbar@gmail.com>
2022-03-20 14:21:44 +0600

    done simple mic

--- BLAHUB_SNAPSHORT/course-service/src/main/java/com/imranmadbar/home/HomeController.java
+++ BLAHUB_SNAPSHORT/course-service/src/main/java/com/imranmadbar/home/HomeController.java
@@ -3,13 +3,23 @@ package com.imranmadbar.home;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @Value("${simple.envVal}")
+    private String simpleEnvVal;
+
     Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @GetMapping(value="/env-val")
+    public String checkEnv(){
+        logger.info("SimpleEnvVal: "+simpleEnvVal);
+        return "SimpleEnvVal: "+simpleEnvVal;
+    }
 
     @GetMapping(value="/")
     public String appIndex(){
